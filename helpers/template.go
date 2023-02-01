89d69ef071fe0b4aa286533076911cbea9344dd2
Michael van Tellingen <michaelvantellingen@gmail.com>
2023-02-01 21:12:28 +0100

    Add go template helpers for rendering hcl files
    This allows us to move from Pongo2 to Go template files

--- helpers/template.go
+++ helpers/template.go
@@ -2,17 +2,15 @@ package helpers
 
 import (
 	"bytes"
+	"reflect"
+	"strings"
 	"text/template"
 )
 
 func RenderGoTemplate(t string, data any) (string, error) {
 	tpl, err := template.
 		New("template").
-		Funcs(map[string]any{
-			"renderProperty": func(key string, value any) string {
-				return SerializeToHCL(key, value)
-			},
-		}).
+		Funcs(TemplateFuncs()).
 		Parse(t)
 	if err != nil {
 		return "", err
@@ -23,4 +21,26 @@ func RenderGoTemplate(t string, data any) (string, error) {
 		return "", err
 	}
 	return content.String(), nil
+}
+
+func TemplateFuncs() map[string]any {
+	return map[string]any{
+		"hasPrefix": strings.HasPrefix,
+		"toLower":   strings.ToLower,
+		"slugify":   Slugify,
+		"renderProperty": func(key string, value any) string {
+			return strings.TrimSuffix(SerializeToHCL(key, value), "\n")
+		},
+		"renderOptionalProperty": func(key string, value any) string {
+			v := reflect.ValueOf(value)
+			if v.Kind() == reflect.String && v.IsZero() {
+				return ""
+			}
+			if v.Kind() == reflect.Slice && v.IsNil() {
+				return ""
+			}
+
+			return strings.TrimSuffix(SerializeToHCL(key, value), "\n")
+		},
+	}
 }
